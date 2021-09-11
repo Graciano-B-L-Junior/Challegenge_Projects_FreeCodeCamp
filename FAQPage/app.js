ce111ef9e067a2fdf75370fcbe17adea3dec7d67
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2021-09-10 21:37:05 -0300

    adicionado mais um projeto FAQ page

--- FAQPage/app.js
+++ FAQPage/app.js
@@ -1,2 +1,25 @@
 
 const buttons = document.querySelectorAll('.cardInfoShow')
+const cardInfo = document.querySelectorAll('.card-info')
+btnClosed = '<i class="far fa-plus-square"></i>'
+btnOpen = '<i class="far fa-minus-square"></i>'
+buttons.forEach((btn, indice) => {
+    btn.addEventListener("click", () => {
+        cardInfo.forEach((val, index) => {
+            if (index == indice) {
+                if(val.classList.contains("show-card")){
+                    btn.innerHTML = btnClosed
+                    val.classList.remove("show-card")
+                }else{
+                    btn.innerHTML = btnOpen
+                    val.classList.add("show-card")
+                }
+                
+            } else {
+                val.classList.remove("show-card")
+                buttons[index].innerHTML = btnClosed
+            }
+        })
+
+    })
+})
