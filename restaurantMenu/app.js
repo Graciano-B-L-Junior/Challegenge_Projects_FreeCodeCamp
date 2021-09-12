dbe96ab27bf344a28b7c0a12753fdb4a58c11670
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2021-09-12 11:49:01 -0300

    adicionado todos os items do restaurante

---
+++ restaurantMenu/app.js
@@ -0,0 +1,4 @@
+let allBtn = document.querySelector('.all_btn')
+let cafeBtn = document.querySelector('.cafe_btn')
+let almocoBtn = document.querySelector('.almoco_btn')
+let sucosBtn = document.querySelector('.sucos_btn')
