0a6801fb52d5d223ea8ffc5c82fccf2331ddd2ac
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2021-09-17 20:03:57 -0300

    quase terminando o projeto countdown timer

---
+++ countdown Clock/app.js
@@ -0,0 +1,22 @@
+let data = new Date();
+
+data.setDate(data.getDate() + 20)
+let dias = document.querySelector('#dias')
+let horas = document.querySelector('#horas')
+let minutos = document.querySelector('#minutos')
+let segundos = document.querySelector('#segundos')
+let x= setInterval(function () {
+    let now = new Date().getTime();
+    let distance = data - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    dias.innerHTML = days;
+    horas.innerHTML=hours;
+    minutos.innerHTML=minutes;
+    segundos.innerHTML = seconds;
+    if(distance<0){
+        clearInterval(x)
+    }
+}, 1000)
