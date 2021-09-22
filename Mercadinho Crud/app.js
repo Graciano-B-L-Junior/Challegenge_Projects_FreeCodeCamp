b235b3ddf5d2a2bdf297c5eb7416e4e82271955d
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2021-09-22 18:47:16 -0300

    função de adicionar e deletar items ao mercadinho concluido

--- Mercadinho Crud/app.js
+++ Mercadinho Crud/app.js
@@ -1,22 +1,35 @@
-function addItem(item) {
-    return `<div class="item">
+let numeroItems = 0;
+
+function addItem(item, indice) {
+    return `<div class="item" id=${indice}>
     <p>${item}</p>
     <div class="editIem">
         <button><i class="fas fa-edit"></i></button>
-        <button><i class="fas fa-trash-alt"></i></button>
+        <button onclick=deleteItem(${indice})><i class="fas fa-trash-alt"></i></button>
     </div>
     </div>`
 }
 
-
 let submit = document.querySelector('#enviar')
 let listaItems = document.querySelector('.items-list')
-submit.addEventListener("click",(e)=>{
+submit.addEventListener("click", (e) => {
     e.preventDefault()
     let valorInput = document.querySelector('#item')
-    if(valorInput.value!=''){
-        let novoResultado = addItem(valorInput.value)
-        listaItems.insertAdjacentHTML('afterbegin',novoResultado)
+    if (valorInput.value != '') {
+        let novoResultado = addItem(valorInput.value, numeroItems)
+        numeroItems++;
+        listaItems.insertAdjacentHTML('afterbegin', novoResultado)
     }
-    valorInput.value=''
+    valorInput.value = ''
 })
+
+function deleteItem(indice) {
+    let item = document.getElementById(indice)
+    item.remove()
+}
+
+function deleteAll(){
+    let elementos = document.querySelectorAll('.item')
+    elementos.forEach(val=>val.remove())
+}
+
